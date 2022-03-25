83c354769571cd0e13105edf8882b29f24090d9f
George Thomas <george.thomas@libertymutual.com>
2022-03-25 14:28:02 -0400

    afterlunchday10

--- code/bashfile1.sh
+++ code/bashfile1.sh
@@ -1,2 +1,10 @@
-#test
-ls -l
+#!/bin/bash
+if test $x = 1; then            
+    echo "x = 1"                        
+else                                     
+    echo "x != 1"                        
+fi                                          
+
+if grep roads poem; then
+    echo "found 'roads'"
+fi
