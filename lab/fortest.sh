5f8c82e76092350319a3e635f5d8e1b278e4ac0e
George Thomas <george.thomas@libertymutual.com>
2022-03-29 08:22:43 -0400

    moring of day 12

--- lab/fortest.sh
+++ lab/fortest.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 for files in "$@"; do
-    #echo $files
+
 if [ -e $files ]; then # -e 'FILE exists'
     if [ -z $files ]; then # -z 'the length of STRING is zero'
         echo 'Please add a file to the end of your command'
     fi
-    if [ -s $files ]; then
+    if [ -s $files ]; then # 'FILE exists and has a size greater than zero'
         echo 'inspected by George Thomas' >> $files 
         > /dev/null # 'redirect output to /dev/null'
         else
