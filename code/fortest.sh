9916aec3797e2de0423e7d7bd19fd84ba01fd7d0
George Thomas <george.thomas@libertymutual.com>
2022-03-29 08:47:56 -0400

    code formater.

--- code/fortest.sh
+++ code/fortest.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 for files in "$@"; do
 
-if [ -e $files ]; then # -e 'FILE exists'
-    if [ -z $files ]; then # -z 'the length of STRING is zero'
-        echo 'Please add a file to the end of your command'
-    fi
-    if [ -s $files ]; then # 'FILE exists and has a size greater than zero'
-        echo 'inspected by George Thomas' >> $files 
-        > /dev/null # 'redirect output to /dev/null'
+    if [ -e $files ]; then     # -e 'FILE exists'
+        if [ -z $files ]; then # -z 'the length of STRING is zero'
+            echo 'Please add a file to the end of your command'
+        fi
+        if [ -s $files ]; then # 'FILE exists and has a size greater than zero'
+            echo 'inspected by George Thomas' >>$files
+            >/dev/null # 'redirect output to /dev/null'
         else
-        rm $files
-        echo 'removed empty file '$files''
-    fi
+            rm $files
+            echo 'removed empty file '$files''
+        fi
 
-else
-    echo 'File '$files' does not exist'
+    else
+        echo 'File '$files' does not exist'
     fi
 done
--- code/ifstatmentlab.sh
+++ code/ifstatmentlab.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ -e $1 ]; then # -e 'FILE exists'
+if [ -e $1 ]; then     # -e 'FILE exists'
     if [ -z $1 ]; then # -z 'the length of STRING is zero'
         echo 'Please add a file to the end of your command'
     fi
-    > /dev/null # 'redirect output to /dev/null'
+    >/dev/null # 'redirect output to /dev/null'
 else
-    echo 'File does not exist' 
+    echo 'File does not exist'
 fi
