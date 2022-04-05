ad056656fe8d61d84fd185828b64b01eb2fc5544
George Thomas <george.thomas@libertymutual.com>
2022-04-05 17:56:13 -0400

    d

--- code/python_scripts/test1.py
+++ code/python_scripts/test1.py
@@ -13,5 +13,14 @@ age = int(0x2B)
 print("RJ is", age)
 """
 
-List = [1, 2, 3, 4, 5]
-print(List[2])
+list = [1, 2, 3, 4, 5]
+list.append(7)
+list.insert(0, 8)
+list.insert(2, "george")
+list.extend([58, "Geeks", "Always"])
+print(list)
+print(list[3:5])
+print(len(list))
+list.reverse()
+
+print(list)
