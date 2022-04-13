d8769bc50dc36ea36cfa8b74253ea43290753ee2
George Thomas <george.thomas@libertymutual.com>
2022-04-13 09:23:19 -0400

    morning 4/13/2022

---
+++ code/python_scripts/ListLessThanTen.py
@@ -0,0 +1,13 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for x in a:
+    if x < 5:
+        print(x)
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
