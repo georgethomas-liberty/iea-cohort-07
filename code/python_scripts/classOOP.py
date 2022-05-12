3ae8b46a1445b5e4504c329ce307a9ea6fa6aca5
George Thomas <george.thomas@libertymutual.com>
2022-05-12 08:13:25 -0400

    morning 5122022

---
+++ code/python_scripts/classOOP.py
@@ -0,0 +1,29 @@
+import operator
+import math
+
+
+class calc:
+    def __init__(self):
+        self.total = 0
+
+    def add(self, num1, num2):
+        self.total = operator.add(num1, num2)
+        return self.total
+
+    def sub(self, num1, num2):
+        self.total = operator.sub(num1, num2)
+        return self.total
+
+    def mult(self, num1, num2):
+        self.total = operator.mul(num1, num2)
+        return self.total2
+
+    def div(self, num1, num2):
+        self.total = operator.truediv(num1, num2)
+        return self.total
+
+    def log(self, num1, num2):
+        result = math.log(self.num1) / math.log(self.num2)
+
+    def pow(self):
+        pass
